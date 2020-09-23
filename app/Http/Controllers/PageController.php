a3c3c6ccd711cb14cf0cbb721eb012aa5c8408d3
Yoann Menard <yoann.menard@epitech.eu>
2020-09-23 18:37:57 +0200

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller {
+    public function Index() {
+        return view('pages.welcome');
+    }
+
+    public function About() {
+        $first = 'Yoann';
+        $last = 'Menard';
+
+        $fullname = $first . " " . $last;
+        $email = 'yoann.menard@epitech.eu';
+
+        $data = [];
+        $data['email'] = $email;
+        $data['fullname'] = $fullname;
+        return view('pages.about')->withData($data);
+    }
+}
+?>
